7b1641d3039dc9bd4384e33d1c5234f3e603c47b
elysiantech <wmondesir@gmail.com>
2024-11-21 18:20:05 -0800

    wrapped component in suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 'use client';
 
+import { Suspense } from 'react';
 import { Playgrounds } from '@/components/playgrounds'
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-background">
+      <Suspense fallback={<div>Loading...</div>}>
       <Playgrounds />
+      </Suspense>
     </main>
   )
 }
