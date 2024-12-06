cad60e8c58a0f695b9efcd8bb7a0dd7bc53eb4cb
elysiantech <wmondesir@gmail.com>
2024-12-06 11:17:23 -0800

    verify signature on callback

--- src/app/api/callback/route.ts
+++ src/app/api/callback/route.ts
@@ -6,26 +6,27 @@ import { verifySignatureAppRouter } from "@upstash/qstash/nextjs";
 const clients = new Map<string, { enqueue: (data: ImageModel) => void; close: () => void }>();
 
 export const POST = verifySignatureAppRouter(async (req: NextRequest) =>{
+// export async function POST (req: NextRequest) {
     const { searchParams } = req.nextUrl;
     const id  = searchParams.get('id');
     const body = await req.json(); 
 
     const messageId = body?.sourceMessageId
-    const result = JSON.parse(Buffer.from(body.body, "base64").toString("ascii"));
+    const buffer = Buffer.from(body.body, "base64").toString()
+    const result = JSON.parse(buffer);
 
     if (!messageId || !result.image_path) {
         return NextResponse.json({ message: 'Invalid payload' })
     }
 
+    const upscaledImageUrl = result.image_path
+    const newImage = await prisma.image.update({
+        where: { id: id as string },
+        data: {
+            url: upscaledImageUrl,
+        },
+    });
     if (clients.has(messageId)) {
-        const upscaledImageUrl = result.image_path
-        const newImage = await prisma.image.update({
-            where: { id: id as string },
-            data: {
-                url: upscaledImageUrl,
-            },
-        });
-        console.log(`Updating message request ${messageId} with result ${upscaledImageUrl}`)
         const controller = clients.get(messageId);
         controller!.enqueue(newImage);
         controller!.close();
